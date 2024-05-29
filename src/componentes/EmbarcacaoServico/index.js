1fdeaa7115e7df38210932ec6f5c1c6e70b5802a
Aluno <Aluno@fatecdiadema.com>
2024-05-29 09:22:18 -0300

    correção serviço

--- src/componentes/EmbarcacaoServico/index.js
+++ src/componentes/EmbarcacaoServico/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const url = "http://localhost:8080/pesqueiros/embarcacoes";
 export const listaDeEmbarcacao = () => axios.get(url);
-export const cadastroDeEmbarcacao = () => axios.get(url);
-export const atualizarEmbarcacao  = () => axios.get(url);
-export const excluirEmbarcacao = ()=> axios.get(url);
-export const buscarEmbarcacao = ()=> axios.get(url);
+export const cadastroDeEmbarcacao = (embarcacao) => axios.post(url, embarcacao);
+export const atualizarEmbarcacao  = (id, embarcacao) => axios.put(url + "/" + id, embarcacao);
+export const excluirEmbarcacao = (id)=> axios.delete(url + "/" + id);
+export const buscarEmbarcacao = (id)=> axios.get(url + "/" + id);
